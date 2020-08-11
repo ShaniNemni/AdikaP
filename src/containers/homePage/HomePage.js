49d89089cbe44cad3bc9b04642ab6fa1987f1c8c
shani nemni <shanemni1@gmail.com>
2020-08-11 13:52:01 +0300

    home page

---
+++ src/containers/homePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdikaNavbar from "../../components/adikaNavbar/AdikaNavbar";
+
+const HomePage = () => {
+
+    return(
+        <AdikaNavbar/>
+    )
+}
+
+export default HomePage;
