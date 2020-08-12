731ff96f4adb7a0cab89049832f601bc03a9e6dc
shani nemni <shanemni1@gmail.com>
2020-08-12 22:36:06 +0300

    adika filter

--- src/components/adikaConditions/adikaFilters/AdikaFilter.js
+++ src/components/adikaConditions/adikaFilters/AdikaFilter.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const AdikaFilter = () => {
+const AdikaFilter = ({filterKey,option}) => {
+    const renderByFilterType = () => {
+        if(filterKey === 'BY COLOR') {
+            return(
+                <span style={{backgroundColor:option}} className={'colors-span'} />
+            )
+        }
+    }
 
+    return (
+        <li className={'li-option'}>
+            {renderByFilterType()}
+            {option}
+        </li>
+    )
 }
 
 export default AdikaFilter;
--- src/components/adikaConditions/adikaFilters/AdikaFilters.js
+++ src/components/adikaConditions/adikaFilters/AdikaFilters.js
@@ -1,44 +1,84 @@
 import React,{useState} from 'react';
+import {FilterOptionsList} from '../../../common/Lists';
+
 import './AdikaFilters.scss';
+import {isMobile} from 'react-device-detect';
 import {FaSortAlphaUpAlt} from 'react-icons/fa';
 import {MdClose} from 'react-icons/md';
+import AdikaDrawer from '../../adikaDrawer/AdikaDrawer';
+import AdikaFilter from './AdikaFilter';
 
 const FILTER = 'Filter';
 const AdikaFilters = () => {
-    const [pressedFilterdMobile,setMobilePressed] = useState(false);
-    const [pressedFilterdWeb,setWebPressed] = useState(false);
+    const [filterPressed,setFilterPressed] = useState(false);
 
-    const toggleMobileFilter = () => {
-        const filterMobilePressedValue = !pressedFilterdMobile;
-        setMobilePressed(filterMobilePressedValue);
+    const toggleFilter = () => {
+        const filterPressedValue = !filterPressed;
+        setFilterPressed(filterPressedValue);
     }
 
-    const toggleWebFilter = () => {
-        const filterWebPressedValue = !pressedFilterdWeb;
-        setMobilePressed(filterWebPressedValue);
-    }
-
-    const mobileIconToDisplay = () => {
-        if(pressedFilterdMobile) {
+    const iconToDisplay = () => {
+        if(!isMobile && filterPressed) {
             return <MdClose/>
         }
-        return <FaSortAlphaUpAlt/>;
+        
+        return <FaSortAlphaUpAlt className={'icon-filter'}/>;
+    }
+
+    const renderOptions = () => {
+        const options = FilterOptionsList.map(filter => {
+            const filter_key = filter.filterType;
+            const values = filter.filterOptions;
+            const filterByType = values.map(option => {
+                return <AdikaFilter key={option} filterKey={filter_key} option={option}/>
+            })
+
+            return(
+                <div key={filter_key}  className={'col-3 col-md-3 filters-view'}>
+                    <h1 className={'filter-key'}>{filter_key}</h1>
+                    <ul className={'filter-block'}>
+                        {filterByType}
+                    </ul>
+                </div>
+            ) 
+
+        })
+
+        return options;
+    }
+
+    const closeDrawer = () => {
+        setFilterPressed(false);
+    }
+
+    const renderByPlatform =() => {
+        const displayFilterOptionsClassname = filterPressed ? '' : 'display-filters-close';
+
+        if(isMobile) {
+            return(
+                <AdikaDrawer drawerPressed={filterPressed} drawerPosition={'left'} toggleDrawer={toggleFilter}>
+                    <div className={'filter-view'}>
+                        <h1 className={'filter-title-drawer'}><MdClose onClick={closeDrawer} className={'filter-md-close'}/>{FILTER}</h1>
+                    </div>
+                        {renderOptions()}
+                </AdikaDrawer>
+            )
+        }
+        return(
+          <div className={displayFilterOptionsClassname  + ' container'}>
+                <div className={'row'}>
+                        {renderOptions()}
+                </div>
+           </div>
+        )
     }
 
     return(
         <div className={'adika-filter-container'}>
-            <div className={'mobile-only'}>
-                <div className={'icon-title-view'} onClick={toggleMobileFilter}>
-                   {mobileIconToDisplay()}
-                  <a className={'filter-title'}>{FILTER}</a>  
-                </div>
+            <div className={'icon-title-view margin-horizontal-5'} onClick={toggleFilter}>
+                <span className={'filter-title'}>{iconToDisplay()}{ FILTER}</span>
             </div>
-            <div className={'desktop-only'}>
-                <div onClick={toggleWebFilter} className={'icon-title-view'}>
-                    <FaSortAlphaUpAlt/>
-                    <a className={'filter-title'}>{FILTER}</a>  
-                </div>
-            </div>
+             {renderByPlatform()}
         </div>
     )
 }
