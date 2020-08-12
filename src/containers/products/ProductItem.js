7aa9e638560fb6556056f598cd9d3f8c2bb11ad1
shani nemni <shanemni1@gmail.com>
2020-08-12 19:11:32 +0300

    product lise

--- src/containers/products/ProductItem.js
+++ src/containers/products/ProductItem.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const ProductItem = ({product}) => {
+    const [isShown, setIsShown] = useState(false);
+    const [imageToDisplay,setImage] = useState(product.images[0]);
+
+    const mouseHover= () => {
+        setIsShown(true);
+        setImage(product.images[1]);
+    }
+
+    const mouseLeave = () => {
+        setIsShown(false);
+        setImage(product.images[0])
+    }
+
+    
     const firstImageToDisplay = product.images[0];
     const secondImageToDisplay = product.images[1];
 
     return(
             <div className={'product-container col-lg-15 col-md-3 col-6'}>
-                <div style={{backgroundImage:`url(${firstImageToDisplay})`,paddingTop:'130%',backgroundSize:'contain',backgroundPosition:'center center',backgroundRepeat:'no-repeat'}}/>
+                <div onMouseOver={mouseHover} onMouseLeave={mouseLeave} class={'image-background'} style={{backgroundImage:`url(${imageToDisplay})`,paddingTop:'130%',backgroundSize:'contain',backgroundPosition:'center center',backgroundRepeat:'no-repeat'}}/>
                 <div className={'product-title-price'}> 
                     <h1 className={'product-title'}>{product.name}</h1>
                     <span className={'product-price'}>{product.price + product.currency}</span>
--- src/containers/products/ProductsList.js
+++ src/containers/products/ProductsList.js
@@ -2,8 +2,11 @@ import React,{useState} from 'react';
 import './ProductsList.scss';
 import {ProductListToDelete} from '../../common/ProductListToDelete';
 import ProductItem from './ProductItem';
+import AdikaPagination from '../../components/adikaBadge/adikaPagination/AdikaPagination';
 
 const ProductsList = () => {
+    const [productsList,setProducts] = useState([]);
+    const [productsCount,setPageNumer] = useState(1);
 
     const renderProducts = () => {
       const products = ProductListToDelete.map(product => {
@@ -12,13 +15,24 @@ const ProductsList = () => {
 
         return products;
     }
+
+    const getPagesNumber = () => {
+        const pages = [];
+        for(let i=1 ; i <= productsCount ; i++) {
+            pages.push(i); 
+        }
+        return pages;
+    }
     
-    
+    const pagesNumber = getPagesNumber();
     return(
-        <div className={'products-container container'}>
-            <div className={'row'}>
-                {renderProducts()}
+        <div>
+            <div className={'products-container container'}>
+                <div className={'row'}>
+                    {renderProducts()}
+                </div>
             </div>
+            {productsCount > 1 && <AdikaPagination pagesCount={pagesNumber}/>} 
         </div>
     )
 
