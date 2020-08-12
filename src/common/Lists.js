b7fb0bed55254b4fd9ad4e97e1e85aa75da9896f
shani nemni <shanemni1@gmail.com>
2020-08-12 22:37:20 +0300

    lists

--- src/common/Lists.js
+++ src/common/Lists.js
@@ -9,7 +9,7 @@ export const SortOptionsList = [
 ]
 
 export const FilterOptionsList = [
-    {filterOptions:["Black","White","Cyan","Green","Grey","Pink","Pink Clay","Silver","White Cream","Beige","Blue"],filterType:'BY COLOR'},
+    {filterOptions:["Black","White","Cyan","Green","Grey","Pink","Gold","Silver","Beige","Blue"],filterType:'BY COLOR'},
     {filterOptions:["50-70$","7-50$"],filterType:'BY PRICE'},
     {filterOptions:["L","M","S","XS","Uk 2","Uk 3","Uk 4","XL"],filterType:'BY SIZE'},
     {filterOptions:["Adika","Zara","HM","Gong","Nike","Adidas"],filterType:'BY BRAND'},
