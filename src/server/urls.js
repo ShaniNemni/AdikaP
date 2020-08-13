c6556150b4ee4c4c070b93c1c9f3648fedfe543c
shani nemni <shanemni1@gmail.com>
2020-08-13 20:20:22 +0300

    url - product by category

--- src/server/urls.js
+++ src/server/urls.js
@@ -5,6 +5,6 @@ export const PRODUCTS = '/product';
 
 export const GET_ALL_CATEGOIRES = CATEGORIES + '/allCategories'
 export const GET_ALL_PRODUCTS = (page) => `${PRODUCTS}/allProducts?page=${page}`;
-export const GET_ALL_PROUDCTS_BY_CATEGORIES = (page) => `${PRODUCTS}/allProductsByCategory?page=${page}`;
+export const GET_ALL_PROUDCTS_BY_CATEGORIES = (page,categoryID) => `${PRODUCTS}/allProductsByCategory?page=${page}&categoryID=${categoryID}`;
 export const GET_ALL_PROUDCTS_BY_FILTER = (page,filterType,filterCondition) =>  `${PRODUCTS}/allProductsByFilter?page=${page}&filterType=${filterType}&filterCondition=${filterCondition}`;
 export const GET_ALL_PROUDCTS_BY_SORT = (page,sortById) => `${PRODUCTS}/allProductsBySort?page=${page}&sortBy=${sortById}`;
