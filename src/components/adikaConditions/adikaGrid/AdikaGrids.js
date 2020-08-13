c889a67f182456912d09116c5259d6213d407857
shani nemni <shanemni1@gmail.com>
2020-08-13 20:31:49 +0300

    Adika grid

--- src/components/adikaConditions/adikaGrid/AdikaGrids.js
+++ src/components/adikaConditions/adikaGrid/AdikaGrids.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import {isMobile} from 'react-device-detect';
 import AdikaGrid from './AdikaGrid';
 import './AdikaGrids.scss';
 
 const AdikaGrids = () => {
-    const renderByPlatform = () => {
-        if(isMobile) {
-            return(
-                <div className={'grids-view'}>
-                   <AdikaGrid selected classname={'view-1'}/>
-                   <AdikaGrid selected classname={'view-6'}/>
-                </div>
-            )
-        }
-
         return(
             <div className={'grids-view'}>
-                <AdikaGrid selected classname={'view-6'}/>
-                <AdikaGrid classname={'view-4'}/>
-                <AdikaGrid classname={'view-3'}/>
-                <AdikaGrid classname={'view-5'}/>
-                <AdikaGrid classname={'view-2'}/>
+                <AdikaGrid selected classname={'view-1 mobile-only'}/>
+                <AdikaGrid selected classname={'view-6 '}/>
+                <AdikaGrid classname={'view-4 desktop-only'}/>
+                <AdikaGrid classname={'view-3 desktop-only'}/>
+                <AdikaGrid classname={'view-5 desktop-only'}/>
+                <AdikaGrid classname={'view-2 desktop-only'}/>
             </div>
         )
-    } 
-    return renderByPlatform()
 }
 
 export default AdikaGrids;
